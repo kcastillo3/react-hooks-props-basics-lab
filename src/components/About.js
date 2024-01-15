7eb154e634cf9b621ff46aea9296e26bc808a5aa
Kevin <kevincastillo333@gmail.com>
2024-01-14 20:23:48 -0500

    second commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,9 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {props.bio ? <p>{props.bio}</p> : null} {/* Conditional rendering */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {<Links  githubLink={props.githubLink} linkedinLink={props.linkedinLink} />}
+      <Links githubLink={props.githubLink} linkedinLink={props.linkedinLink} />
     </div>
   );
 }
